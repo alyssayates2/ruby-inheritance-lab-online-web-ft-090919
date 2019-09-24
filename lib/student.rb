a519708552393a5d5fa30dda12d9ed2a4d5086b7
Alyssa Yates <bosslys@icloud.com>
2019-09-24 19:44:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,10 +10,10 @@ class Student
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-    @@all << self 
-  end 
+    @@all << self
+  end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end 
 end
