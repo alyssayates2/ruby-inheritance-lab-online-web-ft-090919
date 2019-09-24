67ecf3ea645b1d479ea669b89f1cadfbb8047a22
Alyssa Yates <bosslys@icloud.com>
2019-09-24 19:42:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,13 @@
-require_relative "./user.rb"
-
-
-class Student
-
-end
+# require_relative "./user.rb"
+#
+#
+# class Student
+#
+# attr_accessor :first_name, :last_name, :all
+#
+# @@all = []
+#
+# def initialize(first_name, last_name)
+#   @first_name = first_name
+#   @last_name = last_name
+# end
